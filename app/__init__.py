16a4bb4d70260ced775a54f42a7c08497efa8cc2
maks <maks20022409@gmail.com>
2023-08-19 16:34:19 +0300

    Create connection with postgresql

--- app/__init__.py
+++ app/__init__.py
@@ -1,8 +1,15 @@
+import config as c
+
 from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
 from .view.index import index
 
 app = Flask(__name__)
 app.register_blueprint(index)
+
+
+app.config['SQLALCHEMY_DATABASE_URI'] = f'{c.DB_NAME}://{c.DB_USER}:{c.DB_PASSWORD}@{c.DB_HOST}:{c.DB_PORT}/{c.DB_DATABASE}'
+db = SQLAlchemy(app)
 
 
 
---
+++ app/config.py
@@ -0,0 +1,20 @@
+# Database
+DB_NAME = 'postgresql'
+DB_HOST = '127.0.0.1'
+DB_PORT = '5432'
+DB_USER = 'maks'
+DB_PASSWORD = '1111'
+DB_DATABASE = 'techterra'
+
+# Server
+HOST = '127.0.0.1'
+PORT = '5000'
+DEBUG_MODE = True
+
+# Keys
+API_KEY = 'key'
+SECRET_KEY = 'key'
+
+
+
+
